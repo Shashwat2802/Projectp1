f3771e64dda4042ed61bbe6e536490ea9c5587d4
Shashwat2802 <shashwat.a.kalra@gmail.com>
2023-03-24 11:20:28 +0530

    The java file was modified

--- Solve.java
+++ Solve.java
@@ -1,5 +1,11 @@
+import java.util.*;
 class Solve{
     public static void main(String[] args) {
-       System.out.println("Hi there ."); 
+       int[] a1=new int[]{1,2,3,-2,-5,-6};
+       int sum=0;
+       for(int i=0;i<a1.length;i++){
+        sum+=a1[i];
+       }
+       System.out.println("The sum is :" +sum);
     }
 }
